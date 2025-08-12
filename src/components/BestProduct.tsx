0f663be35637cc0a23ae64f13e443331c0ecf23d
ynhule96 <Lenhuy10011996@gmail.com>
2025-08-11 23:49:25 -0400

    change the size for images and container for the carousel best product

--- src/components/BestProduct.tsx
+++ src/components/BestProduct.tsx
@@ -1,37 +1,44 @@
 import React from "react";
 import BestProductDetails from "./BestProductDetails";
 import { dis_plants } from "../constants";
-import {Swiper, SwiperSlide} from "swiper/react";
-import 'swiper/css';
-import 'swiper/css/navigation';
-import {Navigation} from 'swiper/modules';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
 function BestProduct() {
   return (
     <div className="w-full ">
-    {/* // <div className="flex flex-nowrap gap-4 p-4"> */}
-    <Swiper
-  modules={[Navigation]}
-  spaceBetween={15}
-  slidesPerView={2}
-  navigation
-  loop
->
-      {dis_plants.slice(0, 10).map(
-        ({ id, plantName, image, details, originalPrice, discountedPrice }) => (
-          // <div key={id} className="flex-shrink-0 w-40">
-             <SwiperSlide key={id}>
-          <BestProductDetails
-            id={id}
-            image={image}
-            plantName={plantName}
-            details={details}
-            originalPrice={originalPrice}
-            discountedPrice={discountedPrice}
-          /></SwiperSlide>
-   
-        )
-      )}</Swiper>
-    {/* // </div> */}
+      <Swiper
+        modules={[Navigation]}
+        spaceBetween={10}
+        slidesPerView={2}
+        navigation
+        loop
+      >
+        {dis_plants
+          .slice(0, 10)
+          .map(
+            ({
+              id,
+              plantName,
+              image,
+              details,
+              originalPrice,
+              discountedPrice,
+            }) => (
+              <SwiperSlide key={id}>
+                <BestProductDetails
+                  id={id}
+                  image={image}
+                  plantName={plantName}
+                  details={details}
+                  originalPrice={originalPrice}
+                  discountedPrice={discountedPrice}
+                />
+              </SwiperSlide>
+            )
+          )}
+      </Swiper>
     </div>
   );
 }
