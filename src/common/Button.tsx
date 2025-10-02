ba117ceb92dec74609481ac320e8073d4494506f
ynhule96 <Lenhuy10011996@gmail.com>
2025-10-01 23:47:14 -0600

    add take quiz button to the plant quiz section

--- src/common/Button.tsx
+++ src/common/Button.tsx
@@ -3,7 +3,7 @@ import { FaShoppingCart } from "react-icons/fa";
 import { ReactNode } from "react";
 import { ShoppingCartIcon } from "@heroicons/react/24/outline";
 interface ButtonProps {
-  btnType?: "add" | "submit" | "cancel" | "explore" | "signup" | "learn_more" | "shop_now" | "customize" | "explore" |"plant_quiz" | "view_all";
+  btnType?: "add" | "submit" | "cancel" | "explore" | "signup" | "learn_more" | "shop_now" | "customize" | "explore" | "plant_quiz" | "view_all";
   url?: string
   onSubmit?: () => void;
   onClick?: () => void;
@@ -17,11 +17,11 @@ const styleMap: Record<string, string> = {
   // explore: "  text-sm border border-cartRed hover:bg-cartRed hover:text-third transition-all duration-300  text-cartRed mt-5 mb-8",
   signup: "bg-primary  text-third w-full max-sm:w-[15rem] hover:text-primary  hover:bg-white border transition-all duration-300 hover:border-primary  font-semibold rounded-3xl cursor-pointer h-10 tablet:text-2xl tablet:h-16 tablet:rounded-full tablet:w-[15rem]",
   learn_more: "bg-white text-primary font-semibold w-full max-sm:w-[15rem] rounded-3xl border border-primary transition-all duration-300 hover:bg-primary  hover:text-white cursor-pointer h-10 pl-1 tablet:text-2xl tablet:h-16 tablet:rounded-full tablet:w-[15rem]",
-  customize : "text-xs text-third bg-cartRed hover:bg-darkCartRed transition-all duration-300 absolute bottom-[1rem] left-[1rem]",
+  customize: "text-xs text-third bg-cartRed hover:bg-darkCartRed transition-all duration-300 absolute bottom-[1rem] left-[1rem]",
   shop_now: "text-xs text-third bg-accent hover:bg-primary transition-all duration-300  absolute bottom-[1rem] left-[1rem]",
-  explore:"text-xs text-lightGreen  ",
-  plant_quiz:"text-xs text-tertiary text-center bg-lightGreen hover:bg-tertiary transition-all duration-300 hover:text-lightGreen",
-  view_all:"text-xs text-primary hover:bg-lightGreen transition-all duration-300 hover:text-tertiary hover:bg-lightGreen"
+  explore: "text-xs text-lightGreen  ",
+  plant_quiz: "text-xs text-tertiary text-center bg-lightGreen hover:bg-tertiary transition-all duration-300 hover:text-lightGreen",
+  view_all: "text-xs text-primary hover:bg-lightGreen transition-all duration-300 hover:text-tertiary hover:bg-lightGreen"
 
 }
 
@@ -32,24 +32,24 @@ const labelMap: Record<string, string> = {
   explore: "Explore Category",
   signup: "Sign Up",
   learn_more: "Learn More 🌻",
-  shop_now: "Shop Now", 
+  shop_now: "Shop Now",
   customize: "Build Now",
-  plant_quiz:"Take Plant Quiz",
+  plant_quiz: "Take Plant Quiz",
   view_all: "View All Plants"
 }
 
-const iconMap: Record<string,ReactNode>={
-  add:<ShoppingCartIcon className="w-5 h-5 text-white-700 hover:text-green-500 transition" />
+const iconMap: Record<string, ReactNode> = {
+  add: <ShoppingCartIcon className="w-5 h-5 text-white-700 hover:text-green-500 transition" />
 }
-const urlMap : Record<string, string> ={
-  add:"/add-item",
-  learn_more:"/products", 
-  signup:"/signup",
-  customize:"/customize",
-  shop_now:"/shop",
-  explore:"/category",
-  plant_quiz:"/plant_quiz",
-  view_all:"/view_all"
+const urlMap: Record<string, string> = {
+  add: "/add-item",
+  learn_more: "/products",
+  signup: "/signup",
+  customize: "/customize",
+  shop_now: "/shop",
+  explore: "/category",
+  plant_quiz: "/plant_quiz",
+  view_all: "/view_all"
 
 }
 
@@ -59,23 +59,23 @@ function Button({ btnType = "add", url, onSubmit, onClick }: ButtonProps) {
   const label = labelMap[btnType] || "Click";
   const targetUrl = url || urlMap[btnType];
 
-  const handleClick = () =>{
-    if(onClick){
+  const handleClick = () => {
+    if (onClick) {
       onClick();
-    }else if(targetUrl){
+    } else if (targetUrl) {
       navigate(targetUrl);
     }
   }
   return (
     <div className="">
       <button className={`p-1 rounded-2xl px-4 mx-0 my-auto cursor-pointer flex flex-row justify-center items-center gap-3 ${btnClasses}`}
-      onClick={handleClick}
+        onClick={handleClick}
       >
-      
-      {iconMap[btnType] && iconMap[btnType]}
+
+        {iconMap[btnType] && iconMap[btnType]}
         <span>{label}</span>
-   
-       </button> 
+
+      </button>
 
     </div>
   );
