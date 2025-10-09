3ef07f3a07362ccddbdde6fa63ef15df498dd373
ynhule96 <Lenhuy10011996@gmail.com>
2025-10-09 01:16:36 -0600

    add the planting_level icon and position

--- src/features/NewProducts/NewProductCard.tsx
+++ src/features/NewProducts/NewProductCard.tsx
@@ -1,10 +1,11 @@
 import Button from "../../common/Button";
-import { FaStar , FaPaw} from "react-icons/fa";
-import { ProductAndInfo,ProductWithCategory } from "../../types/types";
-import { product } from "../../constants";
+import { FaStar, FaPaw } from "react-icons/fa";
+import { BsHandThumbsUp } from "react-icons/bs";
+import { ProductWithCategory } from "../../types/types";
+
 function NewProductCard({ newProduct }: { newProduct: ProductWithCategory }) {
     const cloud_url = import.meta.env.CLOUDINARY_URL || "https://res.cloudinary.com/dvdr5bwc7/image/upload/c_fill,f_auto,q_auto";
-const imgSrc = newProduct.image_url ? `${cloud_url}/${newProduct.image_url}`:`${cloud_url}/v1759276481/mathias-reding-dMhVYCT_xn0-unsplash_xrnswy.jpg`;
+    const imgSrc = newProduct.image_url ? `${cloud_url}/${newProduct.image_url}` : `${cloud_url}/v1759276481/mathias-reding-dMhVYCT_xn0-unsplash_xrnswy.jpg`;
     if (!newProduct) {
         return <p>Loading new products...</p>
     }
@@ -12,44 +13,65 @@ const imgSrc = newProduct.image_url ? `${cloud_url}/${newProduct.image_url}`:`${
         <section className="border rounded-xl w-[calc(100%-2rem)] mx-auto mx-4 my-6 overflow-hidden relative transform transition-shadow duration-300 hover:shadow-lg">
 
             <div>
-                <p>{newProduct.isnewarrival}</p>
+                {
+                    newProduct.isnewarrival && (
+                        <p className="text-third text-xs absolute z-40 border rounded-xl m-4 p-2 bg-primary">New</p>
+                    )
+
+
+                }
+
                 <img
                     className="w-full h-[10rem] object-cover transform transition-transform duration-300 hover:scale-105"
 
                     src={imgSrc} alt="categories-pictures"
                     loading="lazy"
-                /> 
-                 {/* <p>{newProduct.plantinglevel}</p> */}
-              {newProduct.is_pet_friendly && (
-  <p className="absolute top-2 bg-third px-[.3rem] py-[.3rem] border rounded-[50%] ml-2">
-   <FaPaw className="text-xs" />
-  </p>
-)}
+                />
+                <div className="flex absolute top-4  gap-2 right-4">
+
+                    {
+                        newProduct.plantinglevel && (
+                            <p className="bg-third px-[.3rem] py-[.3rem] border rounded-[50%]">
+                                <BsHandThumbsUp className="text-xs" />
+
+                            </p>
+
+                        )
+                    }
+                    {newProduct.is_pet_friendly && (
+                        <p className=" bg-third px-[.3rem] py-[.3rem] border rounded-[50%]">
+                            <FaPaw className="text-xs" />
+                        </p>
+                    )}
+                </div>
             </div>
             <div className="pl-4">
 
-  <h2 className="text-lg font-semibold pt-4">{newProduct.common_name}</h2>
-                 
+                <h2 className="text-lg font-semibold pt-4">{newProduct.common_name}</h2>
+
                 <p className="text-xs mb-4">{newProduct.name}</p>
                 <div className="flex gap-2 justify-between">
                     <div className="flex row gap-1">
-                     <FaStar className="text-yellow-400 w-5 h-5" />
-                    <p>{newProduct.rating}</p></div>
+                        <FaStar className="text-yellow-400 w-5 h-5" />
+                        <p>{newProduct.rating}</p>
+                        <p className="text-grey">({newProduct.num_reviews})</p>
 
-   <div className="flex gap-2 flex-wrap mr-4">
-    {
-       newProduct.size_available.split(",").map((size , index) => (
-        <p className=" p-1 text-[.6rem] border rounded-lg" key={index}>{size.trim()}</p>
-       ))
-    }
-   </div>
+                    </div>
+
+                    <div className="flex gap-2 flex-wrap mr-4">
+                        {
+                            newProduct.size_available.split(",").map((size, index) => (
+                                <p className=" p-1 text-[.6rem] border rounded-lg" key={index}>{size.trim()}</p>
+                            ))
+                        }
+                    </div>
                 </div>
-             
+
                 <div className="flex flex-row justify-start gap-2 my-4">
-               
-                <p className="text-primary font-semibold">{newProduct.discounted_price}</p>
-                 <p className="text-xs line-through">{newProduct.original_price}</p>
-</div>
+
+                    <p className="text-primary font-semibold">{newProduct.discounted_price}</p>
+                    <p className="text-xs line-through">{newProduct.original_price}</p>
+                </div>
             </div>
 
             <div className="flex gap-2 justify-start mb-6 pl-4">
