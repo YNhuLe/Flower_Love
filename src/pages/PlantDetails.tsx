df1f75123cdf6bc58c05bf447c65460d919d9279
ynhule96 <Lenhuy10011996@gmail.com>
2025-11-13 23:41:59 -0700

    add dynamic price and sub images to the detail plant section

--- src/pages/PlantDetails.tsx
+++ src/pages/PlantDetails.tsx
@@ -1,70 +1,78 @@
 import { useEffect, useState } from "react";
-import { BestProductProp ,NewProductProps} from "../types/types";
+import { BestProductProp, NewProductProps } from "../types/types";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import QuantitySelector from "../common/QuantitySelector";
 import AddToCart from "../common/AddToCart";
+import Button from "../common/Button";
+function PlantDetails() {
 
-function PlantDetails(){
-
-const baseUrl = import.meta.env.VITE_BASE_URL || "http://localhost:3000";
-const {id} = useParams();
+    const baseUrl = import.meta.env.VITE_BASE_URL || "http://localhost:3000";
+    const { id } = useParams();
     const [plantInfo, setPlantInfo] = useState<NewProductProps | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
- const cloud_url = import.meta.env.CLOUDINARY_URL || "https://res.cloudinary.com/dvdr5bwc7/image/upload/c_fill,f_auto,q_auto";
-  
-    useEffect(() =>{
-        const fetchPlantInfo = async() => {
-            try{
-const response = await axios.get(`${baseUrl}/allplants/${id}`);
-setPlantInfo(response.data);
-console.log(" Plant info: ", response.data);
+    const cloud_url = import.meta.env.CLOUDINARY_URL || "https://res.cloudinary.com/dvdr5bwc7/image/upload/c_fill,f_auto,q_auto";
 
-            }catch(error: any){
+    useEffect(() => {
+        const fetchPlantInfo = async () => {
+            try {
+                const response = await axios.get(`${baseUrl}/allplants/${id}`);
+                setPlantInfo(response.data);
+                console.log(" Plant info: ", response.data);
+
+            } catch (error: any) {
                 setError(error.message || "Failed to retrieve plant details!")
-            }finally{
+            } finally {
                 setLoading(false);
             }
         };
         fetchPlantInfo();
     }, [id]);
 
-    if(loading) return <p>Loading...</p>;
+    if (loading) return <p>Loading...</p>;
     if (error) return <p>Error...</p>
 
     console.log("Plant info: ", plantInfo);
-    
-    return ( plantInfo &&
-        <section>
-         <div>   <img src={`${cloud_url}/${plantInfo.image_url}`} alt={plantInfo.common_name} />
-        <h2
-        className="font-semibold text-2xl"
-        >{plantInfo.common_name}</h2>
 
-        <p> $ {plantInfo.original_price}</p>
+    return (plantInfo &&
+        <section className="mx-4">
+            <div>
 
-        {/* <QuantitySelector /> */}
-        <AddToCart />
-        <p>{plantInfo.description}</p>
-        <p>{plantInfo.growth_habit}</p>
-        <p>{plantInfo.bloom_info}</p>
-        <p>{plantInfo.fertilizer_info}</p>
-    <p>{plantInfo.humidity_preference}</p>
-        <p>{plantInfo.watering_requirements}</p>
-        <p>{plantInfo.potting_tip}</p>
+                <img className="bg-grey bg-opacity-70 rounded-xl" src={`${cloud_url}/${plantInfo.image_url}`} alt={plantInfo.common_name} />
+                <div className="flex flex-row aligns-between gap-1 mt-2">
+                    <img className="w-20 h-20 border border-grey rounded-xl" src={`${cloud_url}/${plantInfo.image_url}`} alt={plantInfo.common_name} />
+                    <img className="w-20 h-20 border border-grey rounded-xl" src={`${cloud_url}/${plantInfo.image_url}`} alt={plantInfo.common_name} />
+                    <img className="w-20 h-20 border border-grey rounded-xl" src={`${cloud_url}/${plantInfo.image_url}`} alt={plantInfo.common_name} />
+                </div>
+                <h2
+                    className="font-semibold text-2xl"
+                >{plantInfo.common_name}</h2>
+                <div className="flex flex-row justify-start gap-2">
 
-        <p>{plantInfo.soil_type}</p>
-        <p>{plantInfo.tempareture_range}</p>
-    
-        <p>{plantInfo.mature_width}</p>
-        <p>{plantInfo.mature_height}</p></div>
-        <div>
-            <p>{plantInfo.shipping_info}</p>
-            <p>{plantInfo.stock_quantity}</p>
-            <p>{plantInfo.size_available}</p>
-            <p></p>
-        </div>
+                    <p className=" text-xxs text-primary font-semibold">$ {plantInfo.discounted_price}</p>
+                    <p className="text-xs line-through"> $ {plantInfo.original_price}</p></div>
+                {/* <QuantitySelector /> */}
+                <Button btnType="add" price={plantInfo.discounted_price}></Button>
+                <p>{plantInfo.description}</p>
+                <p>{plantInfo.growth_habit}</p>
+                <p>{plantInfo.bloom_info}</p>
+                <p>{plantInfo.fertilizer_info}</p>
+                <p>{plantInfo.humidity_preference}</p>
+                <p>{plantInfo.watering_requirements}</p>
+                <p>{plantInfo.potting_tip}</p>
+
+                <p>{plantInfo.soil_type}</p>
+                <p>{plantInfo.tempareture_range}</p>
+
+                <p>{plantInfo.mature_width}</p>
+                <p>{plantInfo.mature_height}</p></div>
+            <div>
+                <p>{plantInfo.shipping_info}</p>
+                <p>{plantInfo.stock_quantity}</p>
+                <p>{plantInfo.size_available}</p>
+                <p></p>
+            </div>
         </section>
     )
 }
