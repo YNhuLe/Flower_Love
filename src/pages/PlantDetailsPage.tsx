218fac516ce405bb8c3b803d926c1727282d7b7d
ynhule96 <Lenhuy10011996@gmail.com>
2025-11-25 23:45:37 -0700

    add the PlantDetailsPage

---
+++ src/pages/PlantDetailsPage.tsx
@@ -0,0 +1,15 @@
+import SearchBar from "../common/SearchBar";
+import NavBar from "../components/NavBar";
+import PlantDetails from "./PlantDetails";
+
+function PlantDetailsPage(){
+    return (
+        <>
+        <NavBar />
+        <SearchBar />
+        <PlantDetails />
+        </>
+    )
+}
+
+export default PlantDetailsPage;
