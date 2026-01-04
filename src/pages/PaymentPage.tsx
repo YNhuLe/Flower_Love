d0b32e0267f7a6f4abe682b60b9dcde678220fdc
ynhule96 <Lenhuy10011996@gmail.com>
2026-01-03 19:26:50 -0700

    add totalItemCount into the PaymentPage

--- src/pages/PaymentPage.tsx
+++ src/pages/PaymentPage.tsx
@@ -1,16 +1,20 @@
 import SearchBar from "../common/SearchBar";
 import NavBar from "../components/NavBar";
 import ItemsInCart from "../features/ShoppingCart/ItemsInCart";
+import useCartStore from "../hooks/useCartStore";
 
 
 function PaymentPage(){
+const totalItemCount = useCartStore((state) =>
+  state.items.reduce((sum, item) => sum + item.quantity, 0)
+);
 
     return(
         <>
         <NavBar />
         <SearchBar />
         <h1 className="text-xl font-bold m-4 mb-2">Shopping Cart</h1>
-        <h2 className="text-xxs m-4 mt-2">3 items in your cart</h2>
+        <h2 className="text-xxs m-4 mt-2">{totalItemCount} items in your cart</h2>
         <ItemsInCart />
         </>
     )
