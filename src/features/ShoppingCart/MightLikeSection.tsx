958feddfdae8ff9ea9783f7db2265260ab3a7db6
ynhule96 <Lenhuy10011996@gmail.com>
2026-01-05 23:26:07 -0700

    add You might also like section in the payment page

---
+++ src/features/ShoppingCart/MightLikeSection.tsx
@@ -0,0 +1,52 @@
+
+import { useState, useEffect } from "react";
+import useCartStore from "../../hooks/useCartStore";
+import useAllPlants from "../../hooks/useAllPlants";
+import { PlantWithSize } from "../../types/types";
+
+
+
+
+function MightLikeSection() {
+   const cloud_url = import.meta.env.CLOUDINARY_URL || "https://res.cloudinary.com/dvdr5bwc7/image/upload/c_fill,f_auto,q_auto";
+  
+  const { data: plantList,
+    isLoading,
+    isError, error }
+    = useAllPlants();
+
+  const [randomPlants, setRandomPlants] = useState<PlantWithSize[]>([]);
+
+  useEffect(() => {
+    if (!plantList) return;
+
+    const shuffles = [...plantList]
+      .sort(() => Math.random() - 0.5)
+      .slice(0, 4);
+    setRandomPlants(shuffles);
+
+  }, [plantList])
+  return (
+  
+  <section className=" bg-surface-raised/80 p-4 m-4 rounded-xl">
+    <h1>You Might Also Like</h1>
+  <div  className="grid grid-cols-2 gap-3 ">
+
+    {
+      randomPlants.map((random,id) => (
+ <div key={id} className="rounded-xl w-full mb-4">
+      
+        <img className="rounded-2xl w-full object-cover h-32 bg-text-muted/10 p-2" 
+        src={`${cloud_url}/${random.image_url[0]}`} alt={random.common_name} />
+        <p className="mt-2 text-xxs">{random.common_name}</p>
+        <p className="text-amber-600 mt-1 text-xs">${random.sizes[0].original_price}</p>
+      </div>
+      ))
+     }
+      
+      </div>
+      </section>
+  );
+}
+
+export default MightLikeSection;
