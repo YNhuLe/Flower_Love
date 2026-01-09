63f802f75f9253f517ebfdfa6e6608394f943b54
ynhule96 <Lenhuy10011996@gmail.com>
2026-01-08 23:29:19 -0700

    add schema for the form using zod

---
+++ src/schemas/shippingSchema.ts
@@ -0,0 +1,23 @@
+import {z} from "zod";
+
+
+const shippingSchema  = z.object({
+    firstName: z.string().nonempty("First name is required"),
+    lastName: z.string().nonempty("Last name is required"),
+        phone: z.string().nonempty("Phone number is required"),
+        email: z.string().email("Invalid email").nonempty("Email is required"),
+    streetAddress: z.string().nonempty("Address is required"),
+    aptSuit: z.string().optional(),
+    city: z.string().nonempty("City is required."),
+    postalCode: z.string().nonempty("Postal code is required").regex(/^[ABCEGHJKLMNPRSTVXY]\d[ABCEGHJ-NPRSTV-Z] \d[ABCEGHJ-NPRSTV-Z]\d$/, "Invalid Canadian postal code format"),
+    state: z.string().nonempty("State is required"),
+    country: z.string().nonempty("Country is required")
+
+    ,
+shippingMethod: z.enum(["standard", "express", "overnight"], {
+    errorMap:() => ({message: "Shipping method is required."})
+})
+})
+
+export default shippingSchema;
+export type ShippingFormData = z.infer<typeof shippingSchema>;
