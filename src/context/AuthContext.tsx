fedb2bf6842f566a58d45fe6da8d060f420b1216
ynhule96 <Lenhuy10011996@gmail.com>
2026-03-07 22:58:04 -0700

    add react context for user profile

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,39 @@
+import { createContext, ReactNode,useState, useContext, useEffect } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "../firebase/config";
+interface AuthContextType{
+firebaseUser : User | null,
+isAuthReady: boolean;
+}
+
+const AuthContext = createContext<AuthContextType>({
+    firebaseUser : null,
+    isAuthReady: false,
+})
+
+function AuthProvider({children}: {children: ReactNode}){
+    const [firebaseUser, setFirebaseUser] = useState<User | null>(null);
+
+const [isAuthReady, setIsAuthReady] = useState(false);
+
+    useEffect(() =>{
+        const unsub = onAuthStateChanged(auth, (user) =>{
+            setFirebaseUser(user);
+            setIsAuthReady(true)
+        });
+        return unsub;
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{firebaseUser, isAuthReady}}>
+
+
+            {children}
+        </AuthContext.Provider>
+    )
+}
+export function useAuth() {
+  return useContext(AuthContext);
+}
+export {AuthProvider};
+export default AuthContext;
