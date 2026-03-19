2088886ae85bacd6e52ea9ca67bb03b24c5a0eb0
ynhule96 <Lenhuy10011996@gmail.com>
2026-03-18 21:42:42 -0600

    change the profileStore from firebase to auth0

--- src/hooks/useProfileStore.ts
+++ src/hooks/useProfileStore.ts
@@ -1,44 +1,45 @@
 import { create } from "zustand";
 import axios from "axios";
-import {auth} from "../firebase/config";
 
-interface UserProfile{
-id: number;
-uid: string;
-name: string;
-phone_number: string;
+interface UserProfile {
+  id: number;
+  uid: string;
+  name: string;
+  phone_number: string;
 }
-interface UserProfileState{
-profile: UserProfile | null;
-loadProfile: () => Promise<void>;
-updateProfile: (data: Partial<UserProfile>) => Promise<void>;
+interface UserProfileState {
+  profile: UserProfile | null;
+  loadProfile: (getAccessToken: () => Promise<string>) => Promise<void>;
+  updateProfile: (
+    getAccessToken: () => Promise<string>,
+    data: Partial<UserProfile>,
+  ) => Promise<void>;
 }
-    const baseUrl = import.meta.env.VITE_BASE_URL || "http://localhost:3000";
-const useProfileStore = create<UserProfileState>((set, get)=>({
-profile: null,
-loadProfile: async () =>{ 
-  const currentUser = auth.currentUser;
-  if( !currentUser) {
-     console.log("No currentUser yet — skipping loadProfile");
-     return;
-  };
-    const token = await currentUser?.getIdToken();console.log("Token: ", token);
-    const res = await axios.get(`${baseUrl}/users/me`, {
-        headers: {Authorization: `Bearer ${token}`}
-    });
-     set({profile: res.data})
-},
-  updateProfile: async (data: Partial<UserProfile>) => {
-    const currentUser = auth.currentUser;
-    if(!currentUser) return;
-    const token = await currentUser?.getIdToken();
-    if (!token) return;
-    await axios.put(`${baseUrl}/users/me`, data, {
-      headers: { Authorization: `Bearer ${token}` }
-    });
-    set({ profile: { ...get().profile, ...data } as UserProfile });
-  }
-
-}))
+const baseUrl = import.meta.env.VITE_BASE_URL || "http://localhost:3000";
+const useProfileStore = create<UserProfileState>((set, get) => ({
+  profile: null,
+  loadProfile: async (getAccessToken) => {
+    try {
+      const token = await getAccessToken();
+      const res = await axios.get(`${baseUrl}/users/me`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      set({ profile: res.data });
+    } catch (error: any) {
+      console.log("Failed to load profile", error);
+    }
+  },
+  updateProfile: async (getAccessToken, data) => {
+    try {
+      const token = await getAccessToken();
+      await axios.post(`${baseUrl}/users/me`, data, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      set({ profile: { ...get().profile, ...data } as UserProfile });
+    } catch (error: any) {
+      console.log("failed to update user profile ", error);
+    }
+  },
+}));
 
 export default useProfileStore;
