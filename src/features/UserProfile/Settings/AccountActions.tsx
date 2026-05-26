c868143bddf87315f9847d281de955378cbb2f43
ynhule96 <Lenhuy10011996@gmail.com>
2026-05-26 14:24:36 -0600

    add clearProfile handler

--- src/features/UserProfile/Settings/AccountActions.tsx
+++ src/features/UserProfile/Settings/AccountActions.tsx
@@ -18,8 +18,21 @@ import {
     Upload,
     X,
 } from "lucide-react";
+import useProfileStore from "../../../hooks/useProfileStore";
+import { useAuth0 } from "@auth0/auth0-react";
 function AccountActions() {
-
+ const { clearProfile } = useProfileStore();
+    const { logout } = useAuth0();
+    const handleLogout = async () =>{
+        localStorage.clear();
+        sessionStorage.clear();
+        clearProfile();
+        await logout({
+            logoutParams: {
+                returnTo: window.location.origin
+            }
+        });
+    }
     return (
         <>
             <section className="p-4 flex-col items-center  rounded-md bg-surface-card shadow-md w-full mt-6">
@@ -33,7 +46,9 @@ function AccountActions() {
                     <p className="text-xs">Privacy Settings</p>
                 </div>
 
-                <div className="flex gap-2  items-center mt-4 border-[.1rem] border-error-700/20 rounded-xl p-3 hover:bg-error-700/20 cursor-pointer">
+                <div className="flex gap-2  items-center mt-4 border-[.1rem] border-error-700/20 rounded-xl p-3 hover:bg-error-700/20 cursor-pointer"
+                onClick={handleLogout}
+                >
                     <LogOut className="w-3 h-3 text-error-700" />
                     <p className="text-error-700 text-xs">Log Out</p>
                 </div>
