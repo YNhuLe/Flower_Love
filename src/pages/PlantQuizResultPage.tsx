a247b26279616bbfc252550957598e2ff8c10a77
ynhule96 <Lenhuy10011996@gmail.com>
2026-06-03 23:59:05 -0600

    add slug to the url for product quick view

--- src/pages/PlantQuizResultPage.tsx
+++ src/pages/PlantQuizResultPage.tsx
@@ -2,7 +2,7 @@ import BreadCrumbs from "../common/BreadCrumbs";
 import {
   CheckCircle, ArrowRight
 } from 'lucide-react';
-import { useMemo } from "react";
+import { useMemo, useState, useEffect } from "react";
 import AI_PlantQuizHeader from "../features/PlantQuiz/AI_PlantQuizHeader";
 import { useQuiz } from "../context/QuizContext";
 import Button from "../common/Button";
@@ -10,7 +10,10 @@ import SecondaryRecommendedCard from "../features/PlantQuiz/SecondaryRecommended
 import Footer from "../common/Footer";
 import { useNavigate, Link } from "react-router-dom";
 import useCartStore from "../hooks/useCartStore";
+import ChatbotWindow from "../features/Chatbot/ChatbotWindow";
+import axios from "axios";
 function PlantQuizResultPage() {
+  const baseUrl = import.meta.env.VITE_BASE_URL || "http://localhost:3000";
   const cloud_url = import.meta.env.CLOUDINARY_URL || "https://res.cloudinary.com/dvdr5bwc7/image/upload/c_fill,f_auto,q_auto";
   const navigate = useNavigate();
   const { step, setStep, conditions, setConditions, recommendations, setRecommendations, selectedPlant, setSelectedPlant } = useQuiz();
@@ -20,7 +23,34 @@ function PlantQuizResultPage() {
     </div>
   );
 
-  console.log("Recommd: ", recommendations);
+  const [sessionId, setSessionId] = useState<number | null>(null);
+  const currentUser = { id: 123, name: "John Doe" };
+  // console.log("Recommd: ", recommendations);
+
+  useEffect(() => {
+    const loadSession = async () => {
+      try {
+        let result;
+        if (currentUser) {
+          //loged in user => fetch result by user_id from the DB
+          result = await axios.get(`${baseUrl}/session/user/${currentUser.id}`)
+        } else {
+          //guest user => get session_id from localStorage
+          const storedId = localStorage.getItem("session_id");
+          if (!storedId) return;
+          result = await axios.get(`${baseUrl}/session/${storedId}`)
+        }
+
+        setSessionId(result.data.session_id);
+        // setRecommendations(result.data.recommendations);
+      } catch (err: any) {
+        console.log("Could not load session: ", err)
+      };
+
+    }
+    loadSession()
+      ;
+  }, []);
   //reset the quiz page
   const resetAnalysis = () => {
     setStep('questionnaire');
@@ -39,8 +69,8 @@ function PlantQuizResultPage() {
 
   }
 
-  const {addToCart} = useCartStore();
-    const handleAddToCart = () => {
+  const { addToCart } = useCartStore();
+  const handleAddToCart = () => {
     if (!recommendations[0].id) return;
     addToCart({
       product_id: Number(recommendations[0].id),
@@ -55,7 +85,7 @@ function PlantQuizResultPage() {
     })
     navigate("/products/cart")
   }
-  const isOutOfStock =  recommendations[0].stock_quantity <= 0;
+  const isOutOfStock = recommendations[0].stock_quantity <= 0;
   const firstRecom = recommendations[0];
   let formatArr: string[] = [];
   if (Array.isArray(firstRecom.benefits)) {
@@ -78,17 +108,21 @@ function PlantQuizResultPage() {
     "from-amber-400 via-orange-500 to-yellow-600"
   ];
   const randomGradient = gradients[Math.floor(Math.random() * gradients.length)];
-    const selectedGradient = useMemo(() => randomGradient, []);
-    console.log(firstRecom.sizes[0].discount_percentage);
-    
-    
+  const selectedGradient = useMemo(() => randomGradient, []);
+  // console.log(firstRecom.sizes[0].discount_percentage);
+
+
+
+
   return (
-    <>
+    <section>
+    {/* if(!recommendations || recommendations.length === 0){
+    <div> Loading the recommendations...</div>
+    } */}
       <BreadCrumbs />
 
       <AI_PlantQuizHeader />
 
-      {/* //recommendation section */}
       <section>
 
         <div className="flex items-center justify-center mb-12 max-w-md mx-auto">
@@ -120,9 +154,9 @@ function PlantQuizResultPage() {
             </div>
           </div>
 
-          <Link to={`/products/${recommendations[0].id}`}
-      className="group w-full h-full  cursor-pointer m-4  shadow-sm hover:shadow-2xl transition-all duration-500"
-      >
+          <Link to={`/products/${recommendations[0].slug}`}
+            className="group w-full h-full  cursor-pointer m-4  shadow-sm hover:shadow-2xl transition-all duration-500"
+          >
             <div className="relative overflow-hidden h-[15rem] m-4">
               <img
                 className="w-full h-full object-cover transition-all  duration-700 ease-in-out opacity-100 group-hover:scale-110 group-hover:backdrop-blur-sm "
@@ -131,17 +165,17 @@ function PlantQuizResultPage() {
                 alt={firstRecom.common_name} />
               <div
                 className={`absolute  rounded-xl w-full h-full inset-0 z-10 bg-gradient-to-br ${selectedGradient} opacity-0 group-hover:opacity-40 transition-opacity duration-300 pointer-events-none`}
-          />
+              />
               <div className="absolute inset-0 z-20 flex gap-0 items-center justify-center  opacity-0 group-hover:opacity-100 transition-all duration-500 transform scale-90 group-hover:scale-100">
                 <span className="flex  bg-text-inverse/30 py-2 px-3 text-text-inverse font-bold rounded-full text-xs uppercase tracking-[0.2em] shadow-2xl m-0       
             ">
                   Explore
 
-             <ArrowRight className="w-4 h-4 text-text-inverse transition-tranform duration-300 ease-out transform group-hover:translate-x-1.5" />     </span> </div>
+                  <ArrowRight className="w-4 h-4 text-text-inverse transition-tranform duration-300 ease-out transform group-hover:translate-x-1.5" />     </span> </div>
 
             </div>
 
-     
+
           </Link>
 
 
@@ -154,7 +188,7 @@ function PlantQuizResultPage() {
               <span className="text-success-500 text-xs">{firstRecom.plantinglevel}</span>
 
             </div>
-            <p className="text-center text-amber-600">${((firstRecom.sizes[0].original_price)*((100 - firstRecom.sizes[0].discount_percentage)* .01)).toFixed(2)}</p></div>
+            <p className="text-center text-amber-600">${((firstRecom.sizes[0].original_price) * ((100 - firstRecom.sizes[0].discount_percentage) * .01)).toFixed(2)}</p></div>
 
           <div className="m-4">
 
@@ -167,15 +201,14 @@ function PlantQuizResultPage() {
                   <CheckCircle className='text-success-500 w-4 h-4' />
 
                   <p className="text-xs ">{ben}</p>
-
                 </div>
               ))
             }
           </div>
-
           <div className="px-4">
-
-            <Button btnType="add_to_cart" onClick={handleAddToCart} disabled={isOutOfStock}></Button></div>
+            <Button btnType="add_to_cart" onClick={handleAddToCart} disabled={isOutOfStock}></Button>
+            
+            </div>
         </article>
 
         <article className="bg-text-inverse h-fit m-4 rounded-2xl overflow-hidden shadow-lg pb-6">
@@ -183,10 +216,10 @@ function PlantQuizResultPage() {
           {
             recommendations.slice(1, 4).map((recoms, id) => (
 
-              <Link to={`/products/${recoms.id}`} className="cursor-pointer hover:shadow-lg ">
+              <Link to={`/products/${recoms.slug}`} className="cursor-pointer hover:shadow-lg ">
 
                 <SecondaryRecommendedCard secondRecom={recoms} key={id} />
-                
+
               </Link>
             ))
           }</article>
@@ -195,9 +228,10 @@ function PlantQuizResultPage() {
         </div>
       </section>
 
+      {sessionId && <ChatbotWindow sessionId={sessionId} userId={123} />}
       <Footer />
-  
-    </>
+
+    </section>
   )
 }
 export default PlantQuizResultPage;
