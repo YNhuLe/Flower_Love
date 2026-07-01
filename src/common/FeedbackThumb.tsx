968d79032fc40ab512f2b82fc57a81ac459d5a63
ynhule96 <Lenhuy10011996@gmail.com>
2026-06-30 23:51:56 -0600

    change the style for the Feedback buttons

--- src/common/FeedbackThumb.tsx
+++ src/common/FeedbackThumb.tsx
@@ -14,23 +14,32 @@ interface FeedbackThumbProps {
 
 
 const hoverStyle: Record<'up' | 'down', string> = {
-  up: "hover:border-success-700 hover:text-success-700 cursor-pointer",
+  up: "hover:border-brand-700 hover:text-brand-900 cursor-pointer",
   down: "hover:border-error-700 hover:text-error-700 cursor-pointer",
 }
+
+/***
+ * FeedbackThumb component renders a thumbs up or thumbs down button for user feedback.
+ * @param feedback - The type of feedback, either "up" or "down".
+ *  @param selected - A boolean indicating if the feedback is currently selected.
+ * @param disabled - A boolean indicating if the button is disabled.
+ * @param onClick - A callback function to handle click events on the button.
+ * @returns A JSX element representing the FeedbackThumb component.
+ */
 function FeedbackThumb({ feedback, onClick, selected, disabled }: FeedbackThumbProps) {
 
   const activeStyle =
-    feedback === "up" ?  "bg-brand-900 text-text-inverse border-brand-900" : "bg-error-500 text-text-inverse border-error-500";
+    feedback === "up" ? "bg-brand-700 text-text-inverse border-brand-700" : "bg-error-500 text-text-inverse border-error-500";
 
 
-  const inActive = `bg-surface-card border-text-muted text-text-muted ${hoverStyle[feedback]}`
+  const inActive = `bg-transparent text-text-muted ${hoverStyle[feedback]}`
 
   const Icon = feedback === "up" ? ThumbsUp : ThumbsDown;
   return (
 
     <button
       className={`flex items-center gap-1 px-2 py-1 rounded-full transition-all duration-300
-        border disabled:opacity-60 focus:outline-none
+        border  focus:outline-none
         ${selected ? activeStyle : inActive}`}
       onClick={(e) => {
         e.stopPropagation();
@@ -38,11 +47,9 @@ function FeedbackThumb({ feedback, onClick, selected, disabled }: FeedbackThumbP
         onClick();
       }}
       disabled={disabled}
-      aria-pressed={selected}
+      aria-pressed={true}
     >
-      <Icon size={12} aria-hidden="true"  className={`transition-all ${
-    selected ? "text-white scale-110" : "text-text-muted"
-  }`} />
+      <Icon size={12} aria-hidden="true" className={`transition-all ${selected ? "scale-110" : ""}`} />
     </button>
   );
 }
