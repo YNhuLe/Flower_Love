10ba49b1a8d927195154427fe7b237c91bb19b4e
ynhule96 <Lenhuy10011996@gmail.com>
2026-08-22 23:43:19 -0600

    modify the Checkout page with Stripe

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,46 +1,65 @@
 import ShippingInfoForm from "../features/Checkout/ShippingInfoForm";
-import {Box, ChevronLeft } from "lucide-react";
+import { Box, ChevronLeft, Search } from "lucide-react";
 import Button from "../common/Button";
 import { useNavigate } from "react-router-dom";
 import useCartStore from "../hooks/useCartStore";
-
+import PaymentMethods from "../features/UserProfile/Settings/PaymentMethods";
+import OrderSummary from "../features/ShoppingCart/OrderSummary";
+import Footer from "../common/Footer";
+import SearchBar from "../common/SearchBar";
+import NavBar from "../components/NavBar";
+/**
+ * 
+ * @returns A JSX element representing the checkout page,
+ *  which includes the shipping information form, billing address, 
+ * payment methods, order summary, and a button to place the order.
+ * 
+ */
 function CheckoutPage() {
     const navigate = useNavigate();
 
-const totalSpend=   useCartStore((state) =>
-state.getTotal(state.discountCode)
+    const totalSpend = useCartStore((state) =>
+        state.getTotal(state.discountCode)
     )
     return (
-        <section className="mt-8">
+        <>
+            <NavBar />
+            <SearchBar />
 
-            <div className="flex gap-2 items-center">
-                <ChevronLeft className="w-5 h-5 cursor-pointer ml-6"
+            <div className="bg-surface-base">
+                <section className="">
 
-                    onClick={() => navigate(-1)} />
+                    <div className="flex gap-2 items-center pt-4">
+                        <ChevronLeft className="w-5 h-5 cursor-pointer ml-6"
 
-                <p>Back to Cart</p></div>
-            <h1 className="ml-4 text-xs mt-6">Check out</h1>
-            <h2 className="ml-4 text-xs mb-6">Complete your order and bring nature home</h2>
-            <ShippingInfoForm />
+                            onClick={() => navigate(-1)} />
 
-            <div className="rounded-md bg-surface-card p-4 shadow-md w-full m-4 ">
+                        <p>Back to Cart</p></div>
+                    <h1 className="ml-4 text-xs mt-6">Check out</h1>
+                    <h2 className="ml-4 text-xs mb-6">Complete your order and bring nature home</h2>
+                    <ShippingInfoForm />
 
-                <div className="flex gap-2 items-center mb-6">
-                    <p className="p-2 rounded-full bg-icon-amber-600/20">
-                        <Box className="w-5 h-5 text-icon-amber-600" /></p>
-                    <h1>Billing Address</h1>
-                </div>
+                    <div className="rounded-md bg-surface-card p-4 shadow-md w-auto m-4 ">
 
-                <div className="flex gap-2 items-center">
-                    <input type="checkbox" defaultChecked className="checkbox rounded-sm text-text-primary" />
-                    <p className="text-xs">Same as shipping address</p>
+                        <div className="flex gap-2 items-center mb-6">
+                            <p className="p-2 rounded-full bg-icon-amber-600/20">
+                                <Box className="w-5 h-5 text-icon-amber-600" /></p>
+                            <h1>Billing Address</h1>
+                        </div>
 
-                </div>
+                        <div className="flex gap-2 items-center">
+                            <input type="checkbox" defaultChecked className="checkbox rounded-sm text-text-primary" />
+                            <p className="text-xs">Same as shipping address</p>
+
+                        </div>
+                    </div>
+                    <PaymentMethods />
+                    <Button btnType="place_order" price={totalSpend.toFixed(2)} />
+                    <OrderSummary isCheckout={true} />
+                </section>
+                <Footer />
             </div>
-
-            <Button btnType="place_order" price={totalSpend.toFixed(2)}/>
-    
-        </section>
+        </>
     )
 }
 export default CheckoutPage;
